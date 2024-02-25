fd52a9a5fbd0e742575620b25bdeb0ce907f947d
jonathan.lp <jlopezpe@unal.edu.co>
2024-02-25 14:29:10 -0500

    Finding the Webelement to purchase a random item

--- WebAutomation_FinalProject/src/test/java/Pages/MainPage.java
+++ WebAutomation_FinalProject/src/test/java/Pages/MainPage.java
@@ -1,8 +1,15 @@
 package Pages;
 
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+
+import java.util.List;
 
 public class MainPage extends BasePage {
+
+    @FindBy(css = "[data-test*='add-to-cart']")
+    private List<WebElement> addToCartButtons;
 
     public MainPage(WebDriver driver) {
         super(driver);
